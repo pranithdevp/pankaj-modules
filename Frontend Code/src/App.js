44cac890dd39ac09d2286c4a20b74159c208b0ba
pranithdevp <pranithdevp@gmail.com>
2024-11-24 18:38:46 +0530

    App.js
    Purpose: Add dashboard routing to the application.

---
+++ Frontend Code/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
